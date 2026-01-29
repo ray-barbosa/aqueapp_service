eafa79bb38cb86eb4ca9dff7ddd1feebcbd8a887
Ray Krishna <rayanebarbosa.dev@gmail.com>
2026-01-29 20:00:36 -0300

    start: first commit, init spring boot java project

---
+++ src/main/java/com/aqueapp/aqueapp_service/AqueappServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aqueapp.aqueapp_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AqueappServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AqueappServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aqueapp/aqueapp_service/AqueappServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aqueapp.aqueapp_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AqueappServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
